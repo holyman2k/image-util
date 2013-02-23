05bd4e24b955b7eb1cd6adb4e3704846724bffb8
Charlie Wu <charlie.y.wu@gmail.com>
2013-02-24 09:52:24 +1100

    Initial Commit

---
+++ ImageUtil/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageUtil
+//
+//  Created by Charlie Wu on 24/02/13.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
